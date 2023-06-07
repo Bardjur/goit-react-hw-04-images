2e470429479af4b35b9b7224f047e639dfe025b8
Oleksandr <bragardlin@gmail.com>
2023-06-07 15:26:17 +0300

    added keyboard event

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,20 +1,39 @@
+import React from "react";
 import PropTypes from 'prop-types';
 import {createPortal} from 'react-dom';
 import { Overlay, ModalBlock } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-const Modal = ({onClick, imgUrl}) => {
-  return createPortal(
-    (
-      <Overlay onClick={onClick}>
-        <ModalBlock>
-          <img src={imgUrl} alt="" />
-        </ModalBlock>
-      </Overlay>
-    ),
-    modalRoot
-  )
+class Modal extends React.Component {
+  componentDidMount() {
+    document.addEventListener('keydown', this.handlerKey);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handlerKey);
+  }
+
+  handlerKey = (e) => {
+    if (e.key === "Escape") {
+      this.props.onClick();
+    }
+  }
+
+  render() {
+    const {onClick, imgUrl} = this.props;
+    return createPortal(
+      (
+        <Overlay onClick={onClick}>
+          <ModalBlock>
+            <img src={imgUrl} alt="" />
+          </ModalBlock>
+        </Overlay>
+      ),
+      modalRoot
+    )
+  }
+  
 }
 
 Modal.propTypes = {
